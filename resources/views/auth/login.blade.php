7bb3a40bc31e5d9f64063f1fc5077045af72785b
A-Abd <abrar.abdulwahed@gmail.com>
2023-06-19 15:53:19 +0300

    FP-4

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,21 +1,87 @@
-@extends('layouts.app')
-
+@extends('layouts.layout')
 @section('content')
+@section('title')
+    تسجيل دخول
+@endsection
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+    <div class="row justify-conten-center col-md-12 col-sm-12">
+        <form class="" method="POST" action="{{ route('login') }}">
+            @csrf
+            <section class="vh-1 00 gradient-custom">
+                <div class="contai ner py -5 h-1 00">
+                    <div class="row d-flex justify-content-center align-items-center">
+                        <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+                            <div class="card text-dark" style="background-color:#ECEFF4 ; border-radius: 1rem;">
+                                <div class="card-body p-5 text-center">
+                                    <div class="mb-md-2 mt-md-4 pb-5">
+                                        <h2 class="fw-bold mb-2 text-uppercase">تسجيل دخول</h2>
+                                        <p class="text-dark-50 mb-5">تسجيل الدخول لحسابك</p>
+                                        <div class="d-flehhx justify-con tent-center text-center mt-4 pt-1">
+                                            <a class="btn btn-primary mx-2 px-2" style="background-color: #3b5998;"
+                                                href="{{ url('/auth/facebook/redirect') }}" role="button">
+                                                <i class="fab fa-facebook-f mx-2 "></i> تسجيل الدخول عن طريق الفيسبوك
+                                            </a>
+                                            <br>
+                                            <a class="btn btn-primary mt-2" style="background-color: #dd4b39;"
+                                                href="{{ url('/auth/google/redirect') }}" role="button">
+                                                <i class="fab fa-google mx-2 px-2"></i>تسجيل الدخول عن طريق جوجل
+                                            </a>
+                                        </div>
+                                        <p class="mt-3">أو</p>
+                                        <div class="form-outline form-dark mb-3">
+                                            <input type="email" class="form-control py-2" name="email"
+                                                value="{{ old('email') }}" placeholder="البريد الالكتروني " required />
+                                            <label class="form-label" for="email"></label>
+                                        </div>
+                                        <div class="form-outline form-dark mb-3">
+                                            <input type="password" class="form-control py-2" name="password"
+                                                placeholder="كلمة المرور" required />
+                                            <label class="form-label" for="password"></label>
+                                        </div>
+                                        <p class="small mb-5 pb-lg-2"><a class="text-dark-50" href="#!">هل نسيت
+                                                كلمة المرور ؟</a></p>
+                                        <button class="btn btn-dark btn-lg px-5" type="submit">تسجيل الدخول</button>
+                                    </div>
+
+                                    <div class="mt-0">
+                                        <p class="mb-0">ليس لديك حساب <a href="#!"
+                                                class="text-dark-50 fw-bold">إنشاء حساب</a>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </form>
+        {{-- <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                <div class="card-header">{{ __('Register') }}</div>
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
+                    <form method="POST" action="{{ route('register') }}">
                         @csrf
+
+                        <div class="row mb-3">
+                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                                @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
 
                         <div class="row mb-3">
                             <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -29,7 +95,7 @@
                             <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -40,34 +106,24 @@
                         </div>
 
                         <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
 
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
 
                         <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
+                            <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
+                                    {{ __('Register') }}
                                 </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
                             </div>
                         </div>
                     </form>
                 </div>
             </div>
-        </div>
+        </div> --}}
     </div>
 </div>
 @endsection
