0ec0a32449b6455b824ce100a4dbb8fc3426e23e
wolf <ahmedragabyasin2020@gmail.com>
2023-06-25 14:11:31 +0300

    FP-45 users api

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,18 @@
 <?php
 
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// api users endpoint
+Route::prefix('users')->controller(UserController::class)->group(function(){
+    // Route::get('/','index');
+    Route::get('/latest','latest');
+    Route::get('/search','search');
+
+
 });
