d450a6ffd661eb27dfe19052da917caf4a954c6c
A-Abd <abrar.abdulwahed@gmail.com>
2023-07-02 17:00:35 +0300

    FP-14

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,11 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+use App\Models\CustomMessage;
+use Illuminate\Support\Facades\Lang;
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -21,6 +26,19 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        VerifyEmail::toMailUsing(function (object $notifiable, string $url) {
+            return (new MailMessage)
+                ->subject('تفعيل الايميل')
+                ->line(str_replace("userName",$notifiable->name, CustomMessage::where('code', 'verification.message')->first()->text))
+                ->action('تفعيل البريد الالكتروني', $url);
+        });
+
+        ResetPassword::toMailUsing(function (object $notifiable, string $url) {
+            return (new MailMessage)
+                ->subject(Lang::get('Reset Password Notification'))
+                ->line(str_replace("userName",$notifiable->name, CustomMessage::where('code', 'password.reset_message')->first()->text))
+                ->action(Lang::get('Reset Password'), $url)
+                ->line(Lang::get('This password reset link will expire in :count minutes.', ['count' => config('auth.passwords.'.config('auth.defaults.passwords').'.expire')]));
+        });
     }
 }
