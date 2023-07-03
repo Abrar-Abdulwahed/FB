33ca9ffa2c25d8a1eb305c5179a1f37f3cbea623
islam salah <islamsalah95>
2023-07-03 17:38:41 +0300

    FP-150

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,48 @@
-<footer class="bg-light p-5 text-center mt-3">
-    <div> {{ config('app.name') }}© Copyight 2023 </div>
+<section id="sec-2" class="bg-white mt-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-md-4 text-end">
+                <img class="img-fluid" src="https://blog.mostaql.com/wp-content/uploads/2019/01/Mostaql-Logo.png" alt="">
+                <div>
+                    <p>مستقل هو منصّة تصل بين أصحاب المشاريع والمستقلين في العالم العربي. إن كنت صاحب مشاريع تستطيع استخدام مستقل لانجاز مشاريعك من خلال الانترنت بسهولة وأمان وتستطيع كمستقل تصفّح المشاريع الموجودة وإضافة عروضك على المشاريع التي تستطيع إنجازها. يضمن مستقل حقوقك كاملة ويعمل وسيطًا بين صاحب المشروع والمستقل.</p>
+                </div>
+                <div class="clearfix my-2">
+                    <a class="float-end my-sm-2" href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                    <a class="float-end my-sm-2" href="#"><i class="fa-brands fa-twitter"></i></a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6">
+                <h6 class="text-center text-sm-end">مستقل</h6>
+                <ul>
+                    <li>موقع مستقل</li>
+                    <li>وظف المستقلين</li>
+                    <li>إبدا العمل الحر</li>
+                    <li>تصفّح المشاريع</li>
+                </ul>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6">
+                <h6 class="text-center text-sm-end">روابط</h6>
+                <ul>
+                    <li>أضف مشروعك</li>
+                    <li>مستقل للمؤسسات</li>
+                    <li> معرض الاعمال</li>
+                </ul>
+            </div>
+            
+            
+        </div>
+    </div>
+</section>
+
+
+<footer>
+    <div class="container d-md-flex justify-content-between d-sm-block text-sm-center">
+        <ul class="d-inline-flex">
+            <li><a>اتصل بنا</a></li>
+            <li><a>الاسئلة الشائعة</a></li>
+            <li><a>شروط الاستخدام</a></li>
+            <li><a>ضمان الحقوق</a></li>
+        </ul>
+        <p class="text-center text-md-start">مستقل احد مشاريع شركة حسوب</p>
+    </div>
 </footer>
