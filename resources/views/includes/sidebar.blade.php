6dc2288bd692c2f00a987205b6e0ccc04f843c29
fatma <fatma.a.elsaadawy@gmail.com>
2023-07-04 10:06:29 +0300

    FP-102

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -46,7 +46,7 @@
                     </ul>
                 </li>
 
-                @if (\App\Models\Setting::where('name', 'article_enable')?->first()?->value == 'on')
+               {{--  @if (\App\Models\Setting::where('name', 'article_enable')?->first()?->value == 'on') --}}
 
                     <li
                         class="nav-item {{ Route::is('admin.articles.*') || Route::is('admin.articles-categories.*') || Route::is('admin.faqs.*') || Route::is('admin.comments.*') || Route::is('admin.deletedComments') || Route::is('admin.tags.*') || Route::is('admin.pages.*') ? 'menu-open' : '' }}">
@@ -68,7 +68,7 @@
                                     </p>
                                 </a>
                                 <ul class="nav nav-treeview">
-                                    @if (\App\Models\Setting::where('name', 'page_enable')?->first()?->value == 'on')
+                                    @if (\App\Models\Setting::where('name', 'article_enable')?->first()?->value == 'on')
                                     <li
                                         class="nav-item @if (Route::is('admin.articles.*')) ? 'active' : '' bg-primary @endif">
                                         <a class="nav-link" href="{{ route('admin.articles.index') }}">
@@ -131,7 +131,15 @@
             </ul>
 
             </li>
-            @endif
+            {{-- @endif --}}
+
+            <li
+                class="nav-item @if (Route::is('admin.tickets.*')) ? 'active' : '' bg-primary @endif ">
+                <a href={{ route('admin.tickets.index') }} class="nav-link">
+                    <i class="nav-icon fa-solid fa-message"></i>
+                    <p>الدعم الفني</p>
+                </a>
+            </li>
 
             @if (\App\Models\Setting::where('name', 'short_link_enable')?->first()?->value == 'on')
                 <li class="nav-item">
@@ -144,7 +152,7 @@
             @endif
 
             <li
-                class="nav-item {{ Route::is('admin.settings.*') || Route::is('admin.tickets.*') || Route::is('admin.payments.*') || Route::is('admin.TicketsCategory.*') || Route::is('admin.custom-message.*') || Route::is('admin.tags.*') ? 'menu-open' : '' }}">
+                class="nav-item {{ Route::is('admin.settings.*') || Route::is('admin.payments.*') || Route::is('admin.TicketsCategory.*') || Route::is('admin.custom-message.*') || Route::is('admin.tags.*') ? 'menu-open' : '' }}">
                 <a href="#" class="nav-link">
                     <i class="nav-icon fa-solid fa-cog"></i>
                     <p>
@@ -180,7 +188,7 @@
                         </ul>
                     </li>
                     <li
-                        class="nav-item {{ Route::is('admin.TicketsCategory.*') || Route::is('admin.tickets.*') ? 'menu-open' : '' }}">
+                        class="nav-item {{ Route::is('admin.TicketsCategory.*') ? 'menu-open' : '' }}">
                         <a href="#" class="nav-link">
                             <i class="nav-icon fa-solid fa-newspaper"></i>
                             <p>
@@ -190,33 +198,12 @@
                         </a>
                         <ul class="nav nav-treeview">
                             <li
-                                class="nav-item {{ Route::is('admin.TicketsCategory.*') || Route::is('admin.tickets.*') ? 'menu-open' : '' }}">
-                                <a href="#" class="nav-link">
+                                class="nav-item @if (Route::is('admin.TicketsCategory.*')) ? 'active' : '' bg-primary @endif ">
+                                <a href={{ route('admin.TicketsCategory.index') }} class="nav-link">
                                     <i class="nav-icon fa-solid fa-message"></i>
-                                    <p>
-                                        التذاكر
-                                        <i class="fas fa-angle-left right"></i>
-                                    </p>
+                                    <p>قائمة تصنيفات التذاكر</p>
                                 </a>
-                                <ul class="nav nav-treeview">
-
-                                    <li
-                                        class="nav-item @if (Route::is('admin.tickets.*')) ? 'active' : '' bg-primary @endif ">
-                                        <a href={{ route('admin.tickets.index') }} class="nav-link">
-                                            <i class="nav-icon fa-solid fa-message"></i>
-                                            <p>قائمة التذاكر</p>
-                                        </a>
-                                    </li>
-                                    <li
-                                        class="nav-item @if (Route::is('admin.TicketsCategory.*')) ? 'active' : '' bg-primary @endif ">
-                                        <a href={{ route('admin.TicketsCategory.index') }} class="nav-link">
-                                            <i class="nav-icon fa-solid fa-message"></i>
-                                            <p>قائمة تصنيفات التذاكر</p>
-                                        </a>
-                                    </li>
-                                </ul>
                             </li>
-
                         </ul>
                     </li>
                     <li class="nav-item @if (Route::is('admin.payments.*')) ? 'active' : '' bg-primary @endif ">
