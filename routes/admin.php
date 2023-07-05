8ada53f07fc482c3e32b0ba40f94c5033842d448
A-Abd <abrar.abdulwahed@gmail.com>
2023-07-05 09:14:05 +0300

    FP-165

--- routes/admin.php
+++ routes/admin.php
@@ -21,8 +21,14 @@ use App\Http\Controllers\Admin\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('admin')->middleware(['auth', 'check_user'])->as('admin.')->group(function () {
-    Route::resource('settings', SettingController::class)->only('index', 'store');
     Route::get('/', [AdminHomeController::class, 'index'])->name('index')->middleware('check_user');
+
+    //Settings routes
+	Route::prefix('settings')->name('settings.')->group(function () {
+        Route::resource('/', SettingController::class)->only('index', 'store');
+        Route::delete('cleanup', [SettingController::class, 'cleanup'])->name('cleanup');
+    });
+
     Route::resource('custom-message', CustomMessageController::class)->except('show');
     Route::patch('custom-message/{msg}/active', [CustomMessageController::class, 'changeActive'])
         ->name('custom-message.changeActive');
@@ -31,7 +37,6 @@ Route::prefix('admin')->middleware(['auth', 'check_user'])->as('admin.')->group(
     Route::resource('users', UserController::class);
 
     Route::get('/login-activity', [LoginActivity::class, 'index'])->name('login.activity')->middleware('auth');
-    Route::post('settings/resetdb', [App\Http\Controllers\Admin\SettingController::class, 'reset'])->name('settings.reset');
 
     Route::resource('articles', ArticleController::class)->middleware('feature:article');
     Route::resource('comments', ArticleComment::class);
