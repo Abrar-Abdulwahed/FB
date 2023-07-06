2f9f29ef8848441578ed26102f8be357e5a5e592
A-Abd <abrar.abdulwahed@gmail.com>
2023-07-06 13:49:22 +0300

    FP-173

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -30,7 +30,7 @@ class AuthServiceProvider extends ServiceProvider
         if($verifyEmailMsg){
             VerifyEmail::toMailUsing(function (object $notifiable, string $url) use ($verifyEmailMsg) {
                 return (new MailMessage)
-                    ->subject('تفعيل الايميل')
+                    ->subject($verifyEmailMsg->subject)
                     ->line(str_replace("userName", $notifiable->name, $verifyEmailMsg->text))
                     ->action('تفعيل البريد الالكتروني', $url);
             });
@@ -40,7 +40,7 @@ class AuthServiceProvider extends ServiceProvider
         if($resetPasswordMsg){
             ResetPassword::toMailUsing(function (object $notifiable, string $token) use ($resetPasswordMsg) {
                 return (new MailMessage)
-                    ->subject(Lang::get('Reset Password Notification'))
+                    ->subject($resetPasswordMsg->subject)
                     ->line(str_replace("userName", $notifiable->name, $resetPasswordMsg->text))
                     ->action(Lang::get('Reset Password'), url('/').'/password/reset/'.$token)
                     ->line(Lang::get('This password reset link will expire in :count minutes.', ['count' => config('auth.passwords.'.config('auth.defaults.passwords').'.expire')]));
